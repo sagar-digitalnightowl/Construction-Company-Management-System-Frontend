3abdb584e9a58f4014305641381c4cd4b7ec7dea
ravirajdigitalnightowl <raviraj@digitalnightowl.in>
2026-08-04 10:59:08 +0530

    remove commented code

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,33 +1,3 @@
-//  import api from "./axios";
-
-// export const authApi = {
-//     login: (data) => api.post("/auth/login", data),
-
-//     getProfile: () => api.get("/auth/profile"),
-
-//     updateProfile: (data) => api.patch("/auth/profile", data),
-
-//     logout: () => api.post("/auth/logout"),
-
-//     refreshToken: () => api.post("/auth/refresh-token"),
-
-//     forgotPassword: (data) => api.post("/auth/forgot-password", data),
-
-//     // Yahan params add kiye hain for pagination, search, etc.
-//     getUsers: (params) => api.get("/auth/users", { params }),
-
-//     registerUser: (data) => api.post("/auth/register", data),
-
-//     verifyOtp: (data) => api.post("/auth/verify-otp", data),
-
-//     getPresignedUrl: (data) => api.post("/auth/upload/presigned-url", data),
-
-//     confirmUpload: (data) => api.post("/auth/upload/confirm", data),
-// };
-
-
-
-
 
 // src/api/authApi.js
 
